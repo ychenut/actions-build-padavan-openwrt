79c0679c4118d5f1b307b3bacc19e7e1df55bca9
ychenut <81278909+ychenut@users.noreply.github.com>
2021-10-04 06:16:29 +0800

    Update G-DOCK.sh

--- openwrt/G-DOCK/G-DOCK.sh
+++ openwrt/G-DOCK/G-DOCK.sh
@@ -3,6 +3,6 @@ device_name='ZTE8820S'
 echo "修改机器名称"
 sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
 
-wifi_name=$device_name
+wifi_name='Canada'
 echo "修改wifi名称"
 sed -i "s/OpenWrt/$wifi_name/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
